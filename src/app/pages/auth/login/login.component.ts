17839927348302df291ce996f71db91552ade308
Yoann-B-ENI <yoann.battu2024@campus-eni.fr>
2025-03-27 12:28:02 +0100

    formgroup button submit works

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AuthService } from '../../../services/auth.service';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  email: String = "Email"
+  password: String = ""
+  loginForm = new FormGroup({​
+    email: new FormControl('Email', [Validators.required, Validators.email]),​
+    password: new FormControl('', [Validators.required, Validators.minLength(4)])​
+  });​
+
+  private readonly authService: AuthService = inject(AuthService)
+
+  tryToLogin() {
+    console.log(this.loginForm.controls.email.value)
+    console.log(this.loginForm.controls.password.value)
+  }
 
 }
